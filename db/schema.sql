7ad485b1dcfd272f7547f86902172f9f39456894
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-25 18:59:10 -0500

    server connected

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+
+SELECT * FROM r26jce4a2yccij3d.t_burger;CREATE TABLE `t_burger` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `devoured` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
