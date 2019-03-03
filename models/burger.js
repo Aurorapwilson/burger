8185273982d730d844399b6282ef419b4c88c9d3
Princess Wilson <aurorapwilson1@gmail.com>
2019-03-02 21:43:14 -0500

    fixed routes, in process orm.js

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,29 @@
-//Import the ORM to create funtions that will interact with the database
-var orm = require("../config/orm");
+//require orm.js
+let orm = require("../config/orm.js");
+
+//burger object
+var burger = {
+    all: function(cb) {
+      orm.all("burger", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burger", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burger", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+
+
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
+  
